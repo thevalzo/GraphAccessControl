0ac58e95a423664994ded4a0f6bc569bfb25e826
thevalzo <m.valzelli@campus.unimib.it>
2020-02-05 18:26:04 +0100

    change username

--- JanusgraphAccessControl/src/main/java/it/unimib/disco/janusgraphac/application/GraphAccessControl.java
+++ JanusgraphAccessControl/src/main/java/it/unimib/disco/janusgraphac/application/GraphAccessControl.java
@@ -26,7 +26,7 @@ public class GraphAccessControl {
 		 temp_users = g.V().has("label_vp","user").valueMap().toList();
 		 users =  new HashMap<String, Object>();;
 		 for(int i=0;i<temp_users.size();i++) {
-			 String username=temp_users.get(i).get("name").toString().replace("[", "").replace("]", "");
+			 String username=temp_users.get(i).get("username").toString().replace("[", "").replace("]", "");
 			 users.put(username, temp_users.get(i));
 		 }
 	}
@@ -40,9 +40,9 @@ public class GraphAccessControl {
 		}
  		clearance = Integer.parseInt(user.get("level").toString().replace("[", "").replace("]", ""));
  		
- 		List vertices = g.V().has("label_vp","resource").has("level",P.lte(clearance)).aggregate("total").V().has("name","Rita Levi Montalcini").outE().has("label_ep","part_of").inV().outE().has("access_rights","cannot").inV().aggregate("x").repeat(__.outE().has("access_rights","extends_rights").inV().aggregate("x")).until(__.not(__.outE().has("access_rights","extends_rights"))).cap("x").select("total").unfold().where(P.without("x")).id().toList();
-        List exceptions = g.V().has("name",username).outE().has("access_rights","can").inV().aggregate("x").repeat(__.outE().has("access_rights","extends_rights").inV().aggregate("x")).until(__.not(__.outE().has("access_rights","extends_rights"))).has("level",P.lte(clearance)).cap("x").unfold().id().toList();
-        vertices.addAll((List) exceptions);
+ 		List vertices = g.V().has("label_vp","resource").has("level",P.lte(clearance)).aggregate("total").V().has("username",username).outE().has("label_ep","part_of").inV().outE().has("access_rights","cannot").inV().aggregate("x").repeat(__.outE().has("access_rights","extends_rights").inV().aggregate("x")).until(__.not(__.outE().has("access_rights","extends_rights"))).cap("x").select("total").unfold().where(P.without("x")).id().toList();
+ 		List exceptions = g.V().has("username",username).outE().has("access_rights","can").inV().aggregate("x").repeat(__.outE().has("access_rights","extends_rights").inV().aggregate("x")).until(__.not(__.outE().has("access_rights","extends_rights"))).has("level",P.lte(clearance)).cap("x").unfold().id().toList();
+ 		vertices.addAll((List) exceptions);
         GraphTraversalSource sg=graph.traversal().withStrategies(SubgraphStrategy.build().vertices(__.hasId(P.within(vertices))).create());        
         return sg;
  	}
@@ -56,7 +56,7 @@ public class GraphAccessControl {
 		}
  		clearance = Integer.parseInt(user.get("level").toString().replace("[", "").replace("]", ""));
  		
- 		 List vertices = g.V().has("name",username).outE().has("label_ep","part_of").inV().outE().has("access_rights","can").inV().aggregate("x").repeat(__.outE().has("access_rights","extends_rights").inV().aggregate("x")).until(__.not(__.outE().has("access_rights","extends_rights"))).V().has("name",username).outE().has("access_rights","cannot").inV().aggregate("y").repeat(__.outE().has("access_rights","extends_rights").inV().aggregate("y")).until(__.not(__.outE().has("access_rights","extends_rights"))).cap("y").select("x").unfold().where(P.without("y")).has("label_vp","resource").has("level",P.lte(clearance)).id().toList();
+ 		 List vertices = g.V().has("username",username).outE().has("label_ep","part_of").inV().outE().has("access_rights","can").inV().aggregate("x").repeat(__.outE().has("access_rights","extends_rights").inV().aggregate("x")).until(__.not(__.outE().has("access_rights","extends_rights"))).V().has("username",username).outE().has("access_rights","cannot").inV().aggregate("y").repeat(__.outE().has("access_rights","extends_rights").inV().aggregate("y")).until(__.not(__.outE().has("access_rights","extends_rights"))).cap("y").select("x").unfold().where(P.without("y")).has("label_vp","resource").has("level",P.lte(clearance)).id().toList();
 	     GraphTraversalSource sg=graph.traversal().withStrategies(SubgraphStrategy.build().vertices(__.hasId(P.within(vertices))).create());
          return sg;
  	}
--- JanusgraphAccessControl/src/main/java/it/unimib/disco/janusgraphac/application/Runable.java
+++ JanusgraphAccessControl/src/main/java/it/unimib/disco/janusgraphac/application/Runable.java
@@ -11,10 +11,7 @@ import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSo
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
 import org.apache.tinkerpop.gremlin.process.traversal.strategy.decoration.*;
 import org.apache.tinkerpop.gremlin.process.traversal.*;
-<<<<<<< HEAD
 
-=======
->>>>>>> d5399044c239092a80219fd5b9ca96cbbc54f8ad
 
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
@@ -23,16 +20,13 @@ import org.apache.tinkerpop.gremlin.structure.io.graphml.*;
 
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.*;
 import org.janusgraph.core.JanusGraphFactory;
-<<<<<<< HEAD
-=======
 
->>>>>>> d5399044c239092a80219fd5b9ca96cbbc54f8ad
 public class Runable {
 public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				//Graph graph = JanusGraphFactory.open("conf/janusgraph-hbase-es.properties");
 		        try{
-<<<<<<< HEAD
+
 			        Graph graph =  TinkerGraph.open();
 			        graph.io(GraphMLIo.build()).readGraph("C:\\Users\\marco\\Documents\\Workspaces\\JanusgraphAccessControl\\JanusgraphAccessControl\\data\\users-privileges-civilian.graphml");
 			        GraphTraversalSource g=graph.traversal();       
@@ -55,37 +49,5 @@ public static void main(String[] args) {
 			}
 	}
 
-=======
-		        Graph graph =  TinkerGraph.open();
-		        graph.io(GraphMLIo.build()).readGraph("../data/users-privileges-civilian.graphml");
-		        GraphTraversalSource g=graph.traversal();       
-                    
-                    //CIVILIAN
-		        List vertices = g.V().has("label_vp","resource").has("level",P.lte(2)).aggregate("total").V().has("name","Rita Levi Montalcini").outE().has("label_ep","part_of").inV().outE().has("access_rights","cannot").inV().aggregate("x").repeat(__.outE().has("access_rights","extends_rights").inV().aggregate("x")).until(__.not(__.outE().has("access_rights","extends_rights"))).cap("x").select("total").unfold().where(P.without("x")).id().toList();
-		        List exceptions = g.V().has("name","Rita Levi Montalcini").outE().has("access_rights","can").inV().aggregate("x").repeat(__.outE().has("access_rights","extends_rights").inV().aggregate("x")).until(__.not(__.outE().has("access_rights","extends_rights"))).has("level",P.lte(2)).cap("x").unfold().id().toList();
-		        vertices.addAll((List) exceptions);
-		        GraphTraversalSource sg=graph.traversal().withStrategies(SubgraphStrategy.build().vertices(__.hasId(P.within(vertices))).create());
-                    
-                    
-                    
-                 //MILITARY
-                    
-                graph.io(GraphMLIo.build()).readGraph("../data/users-privileges-military.graphml");
-		        GraphTraversalSource g=graph.traversal();     
-             List vertices = g.V().has("name","Rita Levi Montalcini").outE().has("label_ep","part_of").inV().outE().has("access_rights","can").inV().aggregate("x").repeat(__.outE().has("access_rights","extends_rights").inV().aggregate("x")).until(__.not(__.outE().has("access_rights","extends_rights"))).V().has("name","Rita Levi Montalcini").outE().has("access_rights","cannot").inV().aggregate("y").repeat(__.outE().has("access_rights","extends_rights").inV().aggregate("y")).until(__.not(__.outE().has("access_rights","extends_rights"))).cap("y").select("x").unfold().where(P.without("y")).id().toList();
-		       
-		        vertices.addAll((List) exceptions);
-		        GraphTraversalSource sg=graph.traversal().withStrategies(SubgraphStrategy.build().vertices(__.hasId(P.within(vertices))).create());
-                           
-                    
-                    
-		        System.out.println(sg.V().next(10).toString());
-                    
-                    
-                    
-		        }catch (Exception e){
-		        System.out.println(e.toString());
-		        }
-			}
-	}
->>>>>>> d5399044c239092a80219fd5b9ca96cbbc54f8ad
+
+
